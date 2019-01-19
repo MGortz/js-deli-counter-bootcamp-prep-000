c62debfbe2e3406c61fac2d27c5fa6016fa8f697
Mike Gorton <mike.gorton1@gmail.com>
2019-01-19 23:02:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function currentLine(line) {
     return "The line is currently empty.";
   } else {
     for (var i = 0; i < line.length; i++) {
-      lineArray.push(`${i + 1}. ${line[i]}`);
+      lineArray.push(`${i + 1}. ${line[i]}` + " ");
     }
-    return `The line is currently:${lineArray.join()}`;
+    return `The line is currently: ${lineArray.join()}`;
   }
 }
 
