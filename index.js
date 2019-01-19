ce035ed8b666f5abbd1d4e27c559a326b9220473
Mike Gorton <mike.gorton1@gmail.com>
2019-01-19 22:07:58 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function takeANumber(currentLine, newCustomer) {
+  currentLine.push(`${newCustomer}`);
+  if (currentLine.length === 0) {
+  return `Welcome, ${newCustomer}. You are number 1 in line.`;
+  } else {
+    return `Welcome, ${newCustomer}. You are number ${currentLine.length} in line.`;
+  }
+}
+
+function nowServing(currentLine) {
+  if (currentLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  } else {
+      return `Currently serving ${currentLine.splice(0, 1)}.`}
+}
+
+function currentLine(currentLine) {
+  if (currentLine.length === 0) {
+    return "The line is currently empty.";
+  } else if (currentLine.length === 1) {
+    return `The line is currently: 1. ${currentLine[0]}`;
+  }
+}
