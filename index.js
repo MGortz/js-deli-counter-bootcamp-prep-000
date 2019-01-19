79f63fdeedb8fc65a029ace9ba12a6a31c9d77e4
Mike Gorton <mike.gorton1@gmail.com>
2019-01-19 22:43:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(currentLine) {
 function currentLine(currentLine) {
   if (currentLine.length === 0) {
     return "The line is currently empty.";
-  } else if (currentLine.length > 0) {
+  } else if (currentLine.length === 1) {
     var lineArray = [];
     for (var i = 0; i === currentLine.length; i++) {
       lineArray.push(`${currentLine[i]}`);
